75b4c2cf3bdd3443f8e8611af7379620994eb69a
Martin Angeloni <angelonimartin90@gmail.com>
2023-03-16 15:15:49 -0300

    feat: add slider

--- index.js
+++ index.js
@@ -29,3 +29,40 @@ homeButton.addEventListener("click", onClickHome);
 aboutButton.addEventListener("click", onClickAbout);
 servicesButton.addEventListener("click", onClickServices);
 resumeButton.addEventListener("click", onClickResume);
+
+
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const maxSlide = slides.length;
+slides.forEach((slide, index) => slide.style.transform = `translateX(${100 * index}%)`);
+
+const btnRight = document.querySelector(".btn-right");
+const btnLeft = document.querySelector(".btn-left");
+
+const goToSlide = (slideParam) => {
+  slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${100 * (index - slideParam)}%)`
+  })
+}
+
+
+const nextSlide = () => {
+  if (currentSlide === maxSlide - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+  goToSlide(currentSlide);
+}
+
+const prevSlide = () => {
+  if (currentSlide === 0) {
+    currentSlide = maxSlide - 1;
+  } else {
+    currentSlide--;
+  }
+  goToSlide(currentSlide);
+}
+
+btnRight.addEventListener("click", nextSlide)
+btnLeft.addEventListener("click", prevSlide)
